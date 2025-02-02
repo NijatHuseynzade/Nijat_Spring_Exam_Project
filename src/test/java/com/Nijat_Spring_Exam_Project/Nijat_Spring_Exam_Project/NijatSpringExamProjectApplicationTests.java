bcb153f397ec9e128803be416ca3e30952373a23
Nijat <nicathuseynzade.1303@gmail.com>
2025-02-02 11:39:57 +0400

    Added the file system

---
+++ src/test/java/com/Nijat_Spring_Exam_Project/Nijat_Spring_Exam_Project/NijatSpringExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nijat_Spring_Exam_Project.Nijat_Spring_Exam_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NijatSpringExamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
